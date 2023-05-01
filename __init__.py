e533bcc19adbe82b4a3ec2661bc73250f12f01a6
LorenzoRottigni <lorenzo@rottigni.net>
2023-05-01 18:32:37 +0200

    FEAT: ML6

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from sklearn.datasets import load_wine
 from utils import debug_DF
 # Transform algoritmh used for scaling
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 dataset = load_wine()
 
@@ -17,14 +19,31 @@ X = dataset['data'][
     [phenols_col, magnesium_col]
 ]
 
-debug_DF(X, 'wines_before.png')
+y = dataset['target']
 
+
+""" Scaling data """
+# debug_DF(X, 'wines_before.png')
 # Scaling algorithm
 scale = MinMaxScaler()
 
 # Overwrite entry dataset with scaled data
-X = scale.fit_transform(X)
+s_X = scale.fit_transform(X)
 
-debug_DF(X, 'wines_after.png')
+# Build model with scaled data using KNN (K Nearest Neighbors) algorithm
+s_model = KNeighborsClassifier(n_neighbors=3)
+s_model.fit(s_X, y)
+s_p = s_model.predict(X)
 
+# Unscaled model
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X, y)
+p = model.predict(X)
 
+# debug_DF(X, 'wines_after.png')
+
+s_acc = accuracy_score(y, s_p)
+acc = accuracy_score(y, p)
+
+print('Accuracy not scaled: ', s_acc)
+print('Accuracy scaled: ', acc)
